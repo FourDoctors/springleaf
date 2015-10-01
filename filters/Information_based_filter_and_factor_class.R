dc84c52412f2cc2c2e7e689fd5be16e15fe01734
JMG <jesusmanriquegomez@gmail.com>
2015-10-01 20:19:46 +0200

    Filters

--- filters/Information_based_filter_and_factor_class.R
+++ filters/Information_based_filter_and_factor_class.R
@@ -1,7 +1,7 @@
 load("kld_num_train_df.Rdata")
 df<-kld_num_train_df
 
-##Select informative numeric variables.Informative=high entropy per value or high kld
+ ##Select informative numeric variables.Informative=high entropy per value or high kld
 df<-df[!((df$entropy/log(df$distinct)*log(2))<0.05&df$kld<0.05),]
 
 ##Classify as factor numeric variables with less than 10 distinct values. I add another 
